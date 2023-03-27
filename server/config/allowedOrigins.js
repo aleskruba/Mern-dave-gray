97d18ca5265880832233705f5673d7cb93bdbf97
Ales <aleskruba@seznam.cz>
2023-03-27 16:03:16 +0200

    first commit

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com',
+    'https://dokram.cz'
+]
+
+module.exports = allowedOrigins
